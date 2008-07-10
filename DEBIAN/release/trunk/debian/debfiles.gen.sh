53c14e25c01afd29c97f8fd9cf62aa8c4c9b27cd
romanr <romanr@99f9c56b-b639-0410-8dc2-9496e6a26d34>
2008-07-10 19:41:44 +0000

    debianization files for 0.9.8

---
+++ DEBIAN/release/trunk/debian/debfiles.gen.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+# $Id$
+
+set -e # fail on errors
+#set -x # debug
+
+version=$1
+suffix=$2
+
+[ -z "$version" ] && { echo "USAGE: $0 <version> [suffix]"; exit 1; }
+[ -z "$suffix" ] && exit 0
+
+for pkg in btg-common btg-cli-common btg-cli btg-ncli btg-gui btg-www btg-daemon ; do
+	for file in install init conffiles preinst postinst prerm postrm ; do
+		rm -f ${pkg}${suffix}.$file
+		[ -e $pkg.$file ] && ln $pkg.$file ${pkg}${suffix}.$file
+	done
+done
